292f385fc27e9c2c84bacec60d5a011d74c00493
taku0709 <ai3.ca7@gmail.com>
2021-09-13 12:08:29 +0000

    [finish]3-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -17,15 +18,15 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path
+    redirect_to book_path(book.id)
   end
 
   def destroy
   end
-  
+
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
   resources :books
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
 end
