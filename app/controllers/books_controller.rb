7d754fb107bf59b1a0d6c21f8c5fa9a3ffb90788
taku0709 <ai3.ca7@gmail.com>
2021-09-14 13:12:26 +0000

    [finish]３ー課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,16 +19,24 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if @book.save
+     redirect_to book_path(book.id)
+    else
+      render :new
+    end
+
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
     params.permit(:title, :body)
   end
+
 
 end
