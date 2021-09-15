0fb7b9ba6cc74e79cddb1f3e733e8ce0ecca2ada
taku0709 <ai3.ca7@gmail.com>
2021-09-15 15:05:20 +0000

    [start]3-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
 
   def index
     @books =Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,11 +19,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
 
   end
@@ -32,6 +34,13 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
 
   private
   def book_params
